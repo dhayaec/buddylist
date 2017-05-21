b2cb9ccdc15b3ecba350f0eba4cf27aecdc3d19e
Dhayanandhan <dhayaec@gmail.com>
2017-05-21 18:44:24 +0530

    init buddylist project

---
+++ README.md
@@ -0,0 +1,7 @@
+# buddylist
+
+Simple Group chat app using express mongo and socket.io
+
+* Clone this repo
+* Run 'npm install' or 'yarn'
+* Run 'npm start' or 'nodemon' or 'yarn start'
---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const exphbs = require('express-handlebars');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/node-auth')
+  .then(() => console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+
+function relative(dir) {
+  return path.join(__dirname, dir);
+}
+
+const User = require('./models/User');
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+
+const templateConfig = {
+  partialsDir: relative('/views/partials'),
+  extname: 'html',
+  defaultLayout: relative('views/layout.html'),
+};
+
+app.engine('html', exphbs(templateConfig));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// passport configuration
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
